ef0d1dec59db05ded9681adbeb7c6b31a32b77b8
qkal <77361240+qkal@users.noreply.github.com>
2026-04-23 00:17:55 +0200

    chore: remove rust-era tracked sources

--- crates/shadowsocks-service/src/local/redir/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Shadowsocks Local Transparent Proxy
-
-pub use self::server::{Redir, RedirBuilder};
-
-mod redir_ext;
-mod server;
-mod sys;
-mod tcprelay;
-mod udprelay;
--- src/service/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Service launchers
-
-pub mod genkey;
-#[cfg(feature = "local")]
-pub mod local;
-#[cfg(feature = "manager")]
-pub mod manager;
-#[cfg(feature = "server")]
-pub mod server;
